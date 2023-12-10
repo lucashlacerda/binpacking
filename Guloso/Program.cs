956f5a655b2d1f19bbabdb82abc12d7f50bcdb13
Lucas <contatollacerda@gmail.com>
2023-12-10 18:51:28 -0300

    First commit

---
+++ Guloso/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guloso
+{
+    // Classe para representar um container
+    class Container
+    {
+        public int largura { get; set; }
+        public int altura { get; set; }
+    }
+
+    // Classe para representar um navio
+    class Navio
+    {
+        public int largura { get; set; }
+        public int altura { get; set; }
+        public int capacidade { get; set; }
+        public List<Container> containers { get; set; }
+
+        public Navio(int largura, int altura, int capacidade)
+        {
+            this.largura = largura;
+            this.altura = altura;
+            this.capacidade = capacidade;
+            this.containers = new List<Container>();
+        }
+    }
+
+    // Função para resolver o problema de bin packing com paradigma guloso
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria um navio
+            Navio navio = new Navio(100, 50, 1000);
+
+            // Cria uma lista de containers
+            List<Container> containers = new List<Container>();
+            containers.Add(new Container { largura = 50, altura = 25 });
+            containers.Add(new Container { largura = 25, altura = 25 });
+            containers.Add(new Container { largura = 25, altura = 25 });
+
+            // Resolve o problema de bin packing
+            List<Container> solucao = binPacking(navio, containers);
+
+            // Imprime a solução
+            foreach (Container container in solucao)
+            {
+                Console.WriteLine("Largura: {0}, Altura: {1}, Tamanho: {2}", container.largura, container.altura, container.largura * container.altura);
+            }
+        }
+
+        // Função para resolver o problema de bin packing com paradigma guloso
+        public static List<Container> binPacking(Navio navio, List<Container> containers)
+        {
+            // Ordena os containers pelo tamanho, do menor para o maior
+            containers.Sort((a, b) => a.largura * a.altura - b.largura * b.altura);
+
+            // Adiciona os containers ao navio, um a um
+            for (int i = 0; i < containers.Count; i++)
+            {
+                // Verifica se o container cabe no navio
+                if (navio.largura >= containers[i].largura && navio.altura >= containers[i].altura)
+                {
+                    navio.containers.Add(containers[i]);
+                    navio.largura -= containers[i].largura;
+                    navio.altura -= containers[i].altura;
+                }
+            }
+
+            return navio.containers;
+        }
+    }
+}
+
---
+++ ProgramacaoDinamica/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgramacaoDinamica
+{
+    public class Navio
+    {
+        public int Largura { get; set; }
+        public int Comprimento { get; set; }
+
+        public Navio(int largura, int comprimento)
+        {
+            Largura = largura;
+            Comprimento = comprimento;
+        }
+    }
+
+    public class Container
+    {
+        public int Largura { get; set; }
+        public int Comprimento { get; set; }
+
+        public Container(int largura, int comprimento)
+        {
+            Largura = largura;
+            Comprimento = comprimento;
+        }
+    }
+
+    public class BinPackingDP
+    {
+        public static int Resolver(Navio navio, List<Container> containers)
+        {
+            // Matriz de soluções
+            int[,] solucoes = new int[navio.Largura + 1, navio.Comprimento + 1];
+
+            // Preenchendo a matriz de soluções
+            for (int i = 0; i <= navio.Largura; i++)
+            {
+                for (int j = 0; j <= navio.Comprimento; j++)
+                {
+                    // Se o navio estiver vazio, a solução é 0
+                    if (i == 0 || j == 0)
+                    {
+                        solucoes[i, j] = 0;
+                    }
+                    // Se o container não cabe no navio, a solução é a mesma da célula anterior
+                    else if (containers[i].Largura > i || containers[i].Comprimento > j)
+                    {
+                        solucoes[i, j] = solucoes[i - 1, j];
+                    }
+                    // Se o container cabe no navio, a solução é a menor entre:
+                    // 1 - A solução da célula anterior
+                    // 2 - A solução da célula (i - containers[i].Largura, j - containers[i].Comprimento) + 1 (adicionando o container)
+                    else
+                    {
+                        solucoes[i, j] = Math.Min(solucoes[i - 1, j], solucoes[i - containers[i].Largura, j - containers[i].Comprimento] + 1);
+                    }
+                }
+            }
+
+            // Retorna a solução
+            return solucoes[navio.Largura, navio.Comprimento];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Navio
+            Navio navio = new Navio(10, 10);
+
+            // Containers
+            List<Container> containers = new List<Container>();
+            containers.Add(new Container(5, 5));
+            containers.Add(new Container(3, 3));
+            containers.Add(new Container(2, 2));
+
+            // Resolvendo o problema
+            int solucao = BinPackingDP.Resolver(navio, containers);
+
+            // Imprimindo a solução
+            Console.WriteLine("Solução: {0}", solucao);
+        }
+    }
+}
