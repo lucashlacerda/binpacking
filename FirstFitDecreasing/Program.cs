029d52cc38a6274a695583ce3124d3fca2c2cc6e
Lucas <contatollacerda@gmail.com>
2023-12-13 14:29:44 -0300

    Ajustando o FFD

--- FirstFitDecreasing/Program.cs
+++ FirstFitDecreasing/Program.cs
@@ -25,84 +25,78 @@ namespace BinPacking
             Largura = largura;
             Comprimento = comprimento;
         }
+    }
 
-        public class Bin
+    public class Bin
+    {
+        public int Largura { get; set; }
+        public int Comprimento { get; set; }
+        public List<Container> Containers { get; set; }
+
+        public Bin(int largura, int comprimento)
         {
-            public int Largura { get; set; }
-            public int Comprimento { get; set; }
-
-            public List<Container> Containers { get; set; }
-
-            public Bin(int largura, int comprimento)
-            {
-                Largura = largura;
-                Comprimento = comprimento;
-                Containers = new List<Container>();
-            }
-
-            public void AddContainer(Container container)
-            {
-                if (Largura >= container.Largura && Comprimento >= container.Comprimento)
-                {
-                    Containers.Add(container);
-                }
-            }
+            Largura = largura;
+            Comprimento = comprimento;
+            Containers = new List<Container>();
         }
 
-        public class BinPackingFFD
+        public void AddContainer(Container container)
         {
-            public static int Resolver(Navio navio, List<Container> containers)
+            if (Largura >= container.Largura && Comprimento >= container.Comprimento)
             {
-                // Ordena os containers em ordem decrescente de tamanho
-                containers.Sort((a, b) => b.Largura * b.Comprimento - a.Largura * a.Comprimento);
+                Containers.Add(container);
+            }
+        }
+    }
 
-                // Lista de bins
-                List<Bin> bins = new List<Bin>();
+    public class BinPackingFFD
+    {
+        public static int Resolver(Navio navio, List<Container> containers)
+        {
+            // Ordena os containers em ordem decrescente de tamanho
+            containers.Sort((a, b) => b.Largura * b.Comprimento - a.Largura * a.Comprimento);
 
-                // Adiciona o primeiro container em um novo bin
-                bins.Add(new Bin(containers[0].Largura, containers[0].Comprimento));
+            // Lista de bins
+            List<Bin> bins = new List<Bin>();
 
-                // Para cada container restante
-                for (int i = 1; i < containers.Count; i++)
+            // Adiciona o primeiro container em um novo bin
+            bins.Add(new Bin(navio.Largura, navio.Comprimento));
+
+            // Para cada container restante
+            for (int i = 1; i < containers.Count; i++)
+            {
+                // Encontra o bin que cabe no container
+                foreach (var bin in bins)
                 {
-                    // Encontra o bin que cabe no container
-                    Bin bin = bins.Find(b => b.Largura >= containers[i].Largura && b.Comprimento >= containers[i].Comprimento);
-
-                    // Se não encontrar nenhum bin, cria um novo
-                    if (bin == null)
+                    if (bin.Largura >= containers[i].Largura && bin.Comprimento >= containers[i].Comprimento)
                     {
-                        bin = new Bin(containers[i].Largura, containers[i].Comprimento);
-                        bins.Add(bin);
+                        bin.AddContainer(containers[i]);
+                        break;
                     }
-
-                    // Adiciona o container ao bin
-                    bin.AddContainer(containers[i]);
                 }
-
-                // Retorna a quantidade de bins
-                return bins.Count;
             }
+
+            // Retorna o número total de bins usados
+            return bins.Count;
         }
+    }
 
-        class Program
+    class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            // Exemplo de uso
+            var navio = new Navio(100, 200);
+            var containers = new List<Container>
             {
-                // Navio
-                Navio navio = new Navio(10, 10);
+                new Container(50, 80),
+                new Container(60, 90),
+                // Adicione mais containers aqui conforme necessário
+            };
 
-                // Containers
-                List<Container> containers = new List<Container>();
-                containers.Add(new Container(5, 5));
-                containers.Add(new Container(3, 3));
-                containers.Add(new Container(2, 2));
-
-                // Resolvendo o problema
-                int solucao = BinPackingFFD.Resolver(navio, containers);
-
-                // Imprimindo a solução
-                Console.WriteLine("Solução: {0}", solucao);
-            }
+            int numeroDeBins = BinPackingFFD.Resolver(navio, containers);
+            Console.WriteLine($"Número de bins usados: {numeroDeBins}");
+            Console.ReadLine();
         }
     }
 }
